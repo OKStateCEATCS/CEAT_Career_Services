02e110d8895f88e252fd0bde4fb809a4b5eacd7f
OKStateCEATCS <ceatcs@okstate.edu>
2024-04-11 12:27:58 -0500

    Removed unecessary libraries -Emory

--- New_JobList.py
+++ New_JobList.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from openpyxl import load_workbook
-from openpyxl.workbook import Workbook, web
 from datetime import date
 import os
 import shutil
